4040bd22dd7f77d2afa51935085469b0b0cffb6e
bingyu <289646686@qq.com>
2016-12-15 15:29:33 +0800

    一些git的修改

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+![image](https://github.com/cabbageTingZhang/SZCalendarPicker/blob/master/calendar.gif)
+
 # SZCalendarPicker
+
+一个日历picker的记录
+
+有用到的同学,麻烦动一下你们的小手给个star,哈哈哈~
+
+里面的代码注释增加的很全面,基本上都可以看懂的,希望对你的学习之路有所帮助…
--- SZCalendarPicker/SZCalendarPicker/Class/SZCalendarPicker.h
+++ SZCalendarPicker/SZCalendarPicker/Class/SZCalendarPicker.h
@@ -5,6 +5,7 @@
 //  Created by Stephen Zhuang on 14/12/1.
 //  Copyright (c) 2014年 Stephen Zhuang. All rights reserved.
 //
+//  https://github.com/cabbageTingZhang   很不要脸的给自己的项目里加上我的github地址
 
 #import <UIKit/UIKit.h>
 
